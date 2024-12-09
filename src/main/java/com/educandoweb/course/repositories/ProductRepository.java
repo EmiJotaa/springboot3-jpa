e010e581a7ce5b656ea7ee3691828006cdce6838
Marco Júnior <mrcez4rr@gmail.com>
2024-12-09 10:16:55 -0300

    Code review and final configurations

--- src/main/java/com/educandoweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{	
-	
+public interface ProductRepository extends JpaRepository<Product, Long>{		
 }
